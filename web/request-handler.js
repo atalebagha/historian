6a88d2e9cc1a09399e6dcb328b5b97409e5cb3af
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-28 17:30:18 -0700

    minor changes

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+//var h-help = require('./http-helpers.js')
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
