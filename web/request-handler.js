09acc5316d27cf9ad58368d25c085b64e3407703
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-29 23:07:10 -0700

    minor updates

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,11 +4,6 @@ var archive = require('../helpers/archive-helpers');
 var httpHelp = require('./http-helpers.js')
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  var actionFunction = httpHelp.makeActionHandler(actions);
-  actionFunction(req,res);
-  //res.end(archive.paths.list);
-};
 
 var actions = {
   'GET': function(req, res){
@@ -29,4 +24,11 @@ var actions = {
   'OPTIONS': function(req, res){
     // httpHelp.sendResponse(res, null);
   }
+};
+
+
+exports.handleRequest = function (req, res) {
+  var actionFunction = httpHelp.makeActionHandler(actions);
+  actionFunction(req,res);
+  //res.end(archive.paths.list);
 };
